414c8b4b3a97549a32c8fd65fb03997cd7ff5a05
Russell Snelgrove <russell.snelgrove@shopify.com>
2026-04-10 22:26:46 -0600

    migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_03_221220) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_11_042401) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -88,6 +88,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_04_03_221220) do
     t.integer "cash", default: 500000, null: false
     t.datetime "created_at", null: false
     t.bigint "game_id", null: false
+    t.integer "net_worth"
     t.string "status", default: "active", null: false
     t.integer "turn_position", null: false
     t.datetime "updated_at", null: false
